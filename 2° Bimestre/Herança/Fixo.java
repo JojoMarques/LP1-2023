575c102894c3c6b9f9a02689c7c3c2f5f7d150a0
Jossana Kelly Tavares Marques <126785824+Tm4rqu3sjo@users.noreply.github.com>
2023-05-25 11:40:49 -0300

    Update Fixo.java

--- 2° Bimestre/Herança/Fixo.java
+++ 2° Bimestre/Herança/Fixo.java
@@ -2,7 +2,7 @@ package gastos;
 
 public class Fixo extends Gastos{ //editei pelo git(fiz ela herdar de Gastos), então se der algum erro...
  
-public fixo(String nome, double valor){
+public Fixo(String nome, double valor){
   super(nome,valor);
 }
   
